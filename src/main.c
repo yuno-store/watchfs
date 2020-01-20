c5faf177d9ab6361635be34581d7a21d8a863dfd
Niyamaka <niyamaka@yuneta.io>
2020-01-20 13:26:55 +0100

    increase memory in watchfs

--- src/main.c
+++ src/main.c
@@ -37,10 +37,10 @@ PRIVATE char variable_config[]= "\
     'environment': {                                                \n\
         'use_system_memory': true,                                  \n\
         'log_gbmem_info': true,                                     \n\
-        'MEM_MIN_BLOCK': 32,                                        \n\
-        'MEM_MAX_BLOCK': 65536,             #^^ 64*K                \n\
-        'MEM_SUPERBLOCK': 131072,           #^^ 128*K               \n\
-        'MEM_MAX_SYSTEM_MEMORY': 1048576,   #^^ 1*M                 \n\
+        'MEM_MIN_BLOCK': 64,                                        \n\
+        'MEM_MAX_BLOCK': 52428800,              #^^  50*M           \n\
+        'MEM_SUPERBLOCK': 52428800,             #^^  50*M           \n\
+        'MEM_MAX_SYSTEM_MEMORY': 2147483648,     #^^ 2*G            \n\
         'console_log_handlers': {                                   \n\
             'to_stdout': {                                          \n\
                 'handler_type': 'stdout',                           \n\
