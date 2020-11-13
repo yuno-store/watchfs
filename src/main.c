0e08fe0af5e6f17261594fa696e7324b1a22212a
Niyamaka <niyamaka@yuneta.io>
2020-11-13 18:33:37 +0100

    v4.2.25 hide istream struct

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@
 #define APP_NAME        "watchfs"
 #define APP_DOC         "Execute a shell command when a file system event occurs"
 
-#define APP_VERSION     "4.2.24"
+#define APP_VERSION     "4.2.25"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 
