266f6094b352a072ca2534ffad0bd84afcdef25e
Niyamaka <niyamaka@yuneta.io>
2020-12-15 14:53:36 +0100

    cli with old persistent-attrs; standalones without persistent attrs

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@
 #define APP_NAME        "watchfs"
 #define APP_DOC         "Execute a shell command when a file system event occurs"
 
-#define APP_VERSION     "4.3.0"
+#define APP_VERSION     "4.3.1"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 
@@ -127,13 +127,18 @@ int main(int argc, char *argv[])
      *------------------------------------------------*/
     helper_quote2doublequote(fixed_config);
     helper_quote2doublequote(variable_config);
-    yuneta_set_gobj_startup_functions(
-        db_load_persistent_attrs,   // dbsimple.c
-        db_save_persistent_attrs,   // dbsimple.c
-        db_remove_persistent_attrs, // dbsimple.c
-        db_list_persistent_attrs,   // dbsimple.
-        command_parser,             // command_parser.c
-        stats_parser                // stats_parser.c
+    yuneta_setup(
+        dbattrs_startup,
+        dbattrs_end,
+        dbattrs_load_persistent,
+        dbattrs_save_persistent,
+        dbattrs_remove_persistent,
+        dbattrs_list_persistent,
+        command_parser,
+        stats_parser,
+        0,
+        0,
+        0
     );
     return yuneta_entry_point(
         argc, argv,
