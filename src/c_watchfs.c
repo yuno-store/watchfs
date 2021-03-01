6bf544e7b625fb9ce88580c0156f3ede00df9e70
Niyamaka <niyamaka@yuneta.io>
2021-03-01 09:23:14 +0100

    changed mt_future60 by mt_node_tree

--- src/c_watchfs.c
+++ src/c_watchfs.c
@@ -533,7 +533,7 @@ PRIVATE GCLASS _gclass = {
         0, //mt_node_parents,
         0, //mt_node_childs,
         0, //mt_list_instances,
-        0, //mt_future60,
+        0, //mt_node_tree,
         0, //mt_topic_size,
         0, //mt_future62,
         0, //mt_future63,
