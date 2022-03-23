a8c9c313c369be43ce06e360f16cb00869064587
ginsmar <ginsmar@gmail.com>
2022-03-23 07:46:51 +0100

    remove mt_add_child_resource_link and mt_delete_child_resource_link

--- src/c_watchfs.c
+++ src/c_watchfs.c
@@ -493,8 +493,8 @@ PRIVATE GCLASS _gclass = {
         0, //mt_list_resource,
         0, //mt_update_resource,
         0, //mt_delete_resource,
-        0, //mt_add_child_resource_link
-        0, //mt_delete_child_resource_link
+        0, //mt_future21
+        0, //mt_future22
         0, //mt_get_resource
         0, //mt_state_changed,
         0, //mt_authenticate,
